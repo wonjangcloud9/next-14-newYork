709f0e4ed73fa383a0bd8f42d528305e13b44f7f
wonjangcloud9 <venuscwj@naver.com>
2024-03-30 20:25:56 +0900

    :sparkle: feature: 1. bestSeller 컴포넌트에 아이콘 추가 2. body paddingTop 변경

--- app/_components/bestSeller.tsx
+++ app/_components/bestSeller.tsx
@@ -1,9 +1,13 @@
 import styles from "../styles/best-seller.module.css";
+import { FaArrowCircleRight } from "react-icons/fa";
 
 export default function BestSeller({ bestSellerName }: BestSellerProps) {
   return (
     <div className={styles.card}>
-      <div className={styles.cardContent}>{bestSellerName}</div>
+      <div className={styles.cardContent}>
+        {bestSellerName}
+        <FaArrowCircleRight className={styles.arrowIcon} size={16} />
+      </div>
     </div>
   );
 }
